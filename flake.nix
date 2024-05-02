211f49d7c53db0da44e7a9f609a085df4215f856
daru <mica.tui@proton.me>
2024-05-02 21:37:03 +0200

    flake: init

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Scripts for managing nixos systems written in java";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    mavenix.url = "https://github.com/nix-community/mavenix";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = {
+    flake-parts,
+    mavenix,
+    self,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: let
+        mavenix = mavenix.defaultPackage.${system};
+      in {
+        packages = {
+          hm-build = mavenix.buildMaven ./hm-build;
+          nix-rebuild = mavenix.buildMaven ./nix-rebuild;
+        };
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            maven
+          ];
+        };
+      };
+    };
+}
