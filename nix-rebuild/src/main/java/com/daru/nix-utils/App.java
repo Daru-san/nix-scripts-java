b3deb2ce4c5038b3125e07018d67dc334a649b15
daru <mica.tui@proton.me>
2024-05-02 21:29:29 +0200

    java: add sources

---
+++ nix-rebuild/src/main/java/com/daru/nix-utils/App.java
@@ -0,0 +1,13 @@
+package com.daru.nix-utils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
