364795a9cc18cd537979774b2682469c60937ddf
daru <mica.tui@proton.me>
2024-05-03 20:10:40 +0200

    src/flakes: flake usage attempt #1

--- hm-build/src/main/java/com/daru/nixutils/hmBuild/flakes.java
+++ hm-build/src/main/java/com/daru/nixutils/hmBuild/flakes.java
@@ -1,20 +1,30 @@
 package com.daru.nixutils.hmBuild;
 
 import org.apache.commons.cli.*;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 
 public class flakes extends App {
   public static void flakeOptions() {
     // Flake usage
     Options flakeOpts = new Options();
 
+    // Enable flakes
     Option enableFlake = new Option("F", "flake", false, "Enable flake usage");
+
+    // Print help
     Option help = new Option("h", "help", false, "Show flake usage");
+
+    // Updating inputs
     Option updateInputs = new Option("U", "update-inputs", false, "Update all the flake inputs");
     Option flakeInputs = Option.builder("u").longOpt("flake-inputs")
         .argName("config")
         .hasArg()
         .required(true)
         .desc("Update a specific set of onputs").build();
+
+    // Specifying the flake directory
     Option flakeDir = Option.builder("f").longOpt("flake-dir")
         .argName("flake-dir")
         .hasArg()
@@ -22,13 +32,15 @@ public class flakes extends App {
         .desc("Directory where the `flake.nix` is stored").build();
     Option currentDir = new Option("c", "current-dir", false,
         "Search the current working directory for a flake.nix file and use it");
+
+    // Getting user@hostname
     Option autoMode = new Option("a", "auto", false,
-        "Get the userString and use the current working directory");
-    Option userString = Option.builder("s").longOpt("user-string")
-        .argName("user-string")
+        "Get the configuration url automatically and use the current working directory");
+    Option configUrl = Option.builder("s").longOpt("flake-url")
+        .argName("Flake URL")
         .hasArg()
         .required(true)
-        .desc("The user@hostname combination used to build your configuration").build();
+        .desc("The user@hostname (url) of the configuration in the flake.nix file").build();
 
     // Add the options
     flakeOpts.addOption(help);
@@ -38,7 +50,7 @@ public class flakes extends App {
     flakeOpts.addOption(flakeDir);
     flakeOpts.addOption(currentDir);
     flakeOpts.addOption(autoMode);
-    flakeOpts.addOption(userString);
+    flakeOpts.addOption(configUrl);
 
     getDir(flakeDir);
     getFlake(flakeDir, currentDir);
@@ -56,6 +68,58 @@ public class flakes extends App {
     String flakeDirString[] = { flakeDir.getValue(), currentDir.getValue() };
     // cmd = cmdParser.parse(flakeDirs,flakeDir);
 
+  }
+
+  private final static String flakeUrl(Option mode, Option flakeUrl, Options options) {
+    // Get the user and hostname
+    String hostname;
+    String username;
+    String url;
+    if (options.hasOption(mode.getLongOpt())) {
+      return (flakeUrl.getValue());
+    } else {
+      try {
+        Runtime rt = Runtime.getRuntime();
+        String[] command = { "hostname" };
+        Process proc = rt.exec(command);
+
+        BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+        BufferedReader stdError = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+        // Read the output from the command
+
+        String tmpStr = null;
+        while ((tmpStr = stdInput.readLine()) != null) {
+          hostname = "a" + tmpStr;
+        }
+        while ((tmpStr = stdError.readLine()) != null) {
+          System.err.println("Error getting hostname: " + tmpStr);
+        }
+        url = hostname;
+      } catch (IOException e) {
+        System.err.println("Error getting flake url: " + e.getMessage());
+      }
+      try {
+        Runtime rt = Runtime.getRuntime();
+        String[] command = { "echo", "$USER" };
+        Process proc = rt.exec(command);
+
+        BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+        BufferedReader stdError = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+        // Read the output from the command
+
+        String tmpStr = null;
+        while ((tmpStr = stdInput.readLine()) != null) {
+          username = tmpStr;
+        }
+        while ((tmpStr = stdError.readLine()) != null) {
+          System.err.println("Error getting username: " + tmpStr);
+        }
+        url = username + url;
+      } catch (IOException e) {
+        System.err.println("Error getting flake url: " + e.getMessage());
+      }
+      return url;
+    }
   }
 
   private static void getDir(Option flakeDir) {
